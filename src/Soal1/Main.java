69cb27b2d14bd095b8ab23dcc4b2ac4aba8b3148
Falery Mustika <falerymustika@Falerys-MacBook-Pro.local>
2017-05-15 12:31:01 +0700

    set up project

---
+++ src/Soal1/Main.java
@@ -0,0 +1,7 @@
+package Soal1;
+
+/**
+ * Created by falerymustika on 5/15/17.
+ */
+public class Main {
+}
---
+++ src/Soal2/Main.java
@@ -0,0 +1,7 @@
+package Soal2;
+
+/**
+ * Created by falerymustika on 5/15/17.
+ */
+public class Main {
+}
